3e4602de6c2621444f4f598a1c860602e539055a
Andromeda <andrei.froschgaizer@yandex.ru>
2023-04-16 13:45:21 +0700

    fixed bug with notification light theme text color

--- src/features/notification/ui/Notification.tsx
+++ src/features/notification/ui/Notification.tsx
@@ -32,33 +32,29 @@ export const Notification = ({ notification }: NotificationProps) => {
   return (
     <div
       className={clsx(
-        'bg-transparent fade backdrop-blur-sm rounded-lg w-full sm:w-[350px] overflow-hidden',
+        'fade backdrop-blur-sm rounded-lg w-full sm:w-[350px] overflow-hidden',
         type === 'info' &&
-          'text-color-dark bg-primary/70 border border-primary/20 dark:border-secondary/20',
+          'text-color-light dark:text-color-dark bg-primary/70 border border-primary/20 dark:border-secondary/20',
         type === 'error' && 'text-color-dark bg-error/90 dark:bg-error/50',
         type === 'success' && 'text-color-dark bg-green-500/80',
         type === 'warning' && 'text-color-dark bg-yellow-600/70'
       )}
     >
-      <div className='pt-3 pb-3.5 w-full px-4 flex items-center justify-start space-x-4 '>
-        <div>
-          <Icon name={iconName[type]} />
-        </div>
+      <div className='pt-3 pb-3.5 w-full px-4 flex items-center justify-start space-x-4'>
+        <Icon name={iconName[type]} size='xl' />
 
         <div>
           <div className='font-semibold'>{title}</div>
           <div className='text-sm'>{message}</div>
         </div>
 
-        <div className='flex-1 flex justify-end'>
-          <IconButton
-            size='xs'
-            className='!text-white'
-            variant='text'
-            icon='close'
-            onClick={handleCloseNotification}
-          />
-        </div>
+        <IconButton
+          size='xs'
+          className='text-white self-start'
+          variant='text'
+          icon='close'
+          onClick={handleCloseNotification}
+        />
       </div>
 
       <NotificationProgres duration={duration} />
@@ -69,7 +65,7 @@ export const Notification = ({ notification }: NotificationProps) => {
 const NotificationProgres = ({ duration }: { duration: number }) => (
   <div className='absolute bottom-0 left-0 h-1 w-full overflow-hidden'>
     <div
-      className='progress bg-secondary'
+      className='progress bg-slate-300 dark:bg-secondary'
       style={{ animationDuration: `${duration}ms` }}
     ></div>
   </div>
