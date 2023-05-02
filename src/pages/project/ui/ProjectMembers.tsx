0024b00f8a29b6a29b985bbb89dae0f5c5f431f7
Andromeda <andrei.froschgaizer@yandex.ru>
2023-05-02 13:52:46 +0700

    change light color in project members

--- src/pages/project/ui/ProjectMembers.tsx
+++ src/pages/project/ui/ProjectMembers.tsx
@@ -8,7 +8,7 @@ export const ProjectMembers = () => {
   const currentProject = useUnit(currentProjectModel.$currentProject)
 
   return (
-    <div className='flex items-center space-x-2 text-xs text-color-light/60 dark:text-color-dark/50'>
+    <div className='flex items-center space-x-2 text-xs text-color-light dark:text-color-dark/50'>
       <div>{currentProject?.members.length} members</div>
       <AvatarGroup members={currentProject?.members || []} />
 
