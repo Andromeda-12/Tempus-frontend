15107605900b399f343c3322d6cb27e0610d60bd
Andromeda <andrei.froschgaizer@yandex.ru>
2023-05-08 12:02:55 +0700

    created task modal creator component

--- src/widgets/task-modal/ui/TaskCreator.tsx
+++ src/widgets/task-modal/ui/TaskCreator.tsx
@@ -1,3 +1,24 @@
-export const TaskCreator = () => {
-    return <div></div>
+import clsx from 'clsx'
+import { useUnit } from 'effector-react'
+import { currentTaskModel } from '@/entities/current-task'
+import { Avatar } from '@/shared/ui'
+import { getImageUrl } from '@/shared/lib'
+
+export const TaskCreator = ({ className }: { className?: string }) => {
+  const taskCreator = useUnit(currentTaskModel.$taskCreator)
+
+  let fallback = ''
+  if (taskCreator)
+    fallback = taskCreator?.firstName[0] + taskCreator?.lastName[0]
+
+  return (
+    <div className={clsx('flex items-center', className)}>
+      <span className='text-sm mr-5'>Creator</span>
+
+      <div className='flex items-center space-x-3'>
+        <Avatar src={getImageUrl(taskCreator?.avatar)} fallback={fallback} />
+        <span className='text-sm'>{`${taskCreator?.lastName} ${taskCreator?.firstName} `}</span>
+      </div>
+    </div>
+  )
 }
