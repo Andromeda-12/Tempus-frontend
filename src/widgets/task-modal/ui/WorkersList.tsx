30b3633fbbb253e3e62ab84728f0eed20bdd41ca
Andromeda <andrei.froschgaizer@yandex.ru>
2023-05-09 17:03:13 +0700

    fixed variant worker list

--- src/widgets/task-modal/ui/WorkersList.tsx
+++ src/widgets/task-modal/ui/WorkersList.tsx
@@ -1,38 +1,54 @@
-import { ViewerAvatar } from '@/entities/viewer'
-import { Checkbox, Tooltip } from '@/shared/ui'
 import clsx from 'clsx'
-import { ReactNode } from 'react'
+import { useUnit } from 'effector-react'
+import { ViewerAvatar } from '@/entities/viewer'
+import { currentTaskModel, taskManagerModel } from '@/entities/current-task'
+import { Avatar, Checkbox, Spinner, Tooltip } from '@/shared/ui'
+import { formatTime, getImageUrl } from '@/shared/lib'
+import { MembersInfo } from '@/shared/api'
+import { $isLoadingMemberList } from '../model'
 
 export const WorkersList = () => {
+  const isLoading = useUnit($isLoadingMemberList)
+  const currentTask = useUnit(currentTaskModel.$currentTask)
+
+  if (!currentTask || currentTask.members.length === 0)
+    return <div>No assigned</div>
+
   return (
-    <div className='border border-primary dark:border-secondary rounded-lg overflow-hidden h-full w-full flex pr-2 py-2'>
-      <div className='scrollbar scrollbar-dense overflow-y-auto overflow-x-auto w-full pr-1'>
-        <Worker />
-        <Worker1 />
-        <Worker />
-        <Worker1 />
-        <Worker />
+    // <div className='border border-primary dark:border-secondary rounded-lg overflow-hidden h-full w-full flex pr-2 py-2'>
+
+    <div className=' w-[290px] relative overflow-hidden'>
+      {isLoading && (
+        <div className='absolute inset-0 bg-neutral/90 z-20  flex justify-center items-center'>
+          <Spinner className='!w-10 !h-10 border-4' />
+        </div>
+      )}
+
+      <div className='scrollbar scrollbar-dense overflow-y-auto overflow-x-auto w-full rounded-lg h-full max-h-[200px] overflow-hidden'>
+        {currentTask.members.map((member) => (
+          <Member memberInfo={member} />
+        ))}
       </div>
     </div>
+    // {/* </div> 
   )
 }
 
-const Worker = () => {
+const Member = ({ memberInfo }: { memberInfo: MembersInfo }) => {
+  const { member, workTime, isComplete } = memberInfo
+  const { avatar, firstName, lastName } = member
   return (
-    <div className='flex items-center py-1.5 px-4 text-xs md:text-sm space-x-5 w-full'>
+    <div className='flex items-center py-1.5 text-xs md:text-sm space-x-5 w-full'>
       <div className='w-1/12'>
-        <ViewerAvatar />
+        <Avatar size='sm' src={getImageUrl(avatar)} />
       </div>
 
-      <Column
-        className='w-8/12 max-w-[65%] min-w-[65%]'
-        text='Fronshaizer Andrei'
-      />
+      <Column className='w-6/12 ' text={`${lastName} ${firstName}`} />
 
-      <div className='w-1/12 flex justify-center'>12:51</div>
+      <div className='w-3/12 flex justify-end'>{formatTime(workTime)}</div>
 
-      <div className='w-1/12 flex justify-center'>
-        <Checkbox />
+      <div className='w-2/12 flex justify-end'>
+        <Checkbox checked={isComplete} />
       </div>
     </div>
   )
@@ -47,24 +63,3 @@ const Column = ({ className, text }: { className?: string; text: string }) => (
     </Tooltip>
   </div>
 )
-
-const Worker1 = () => {
-  return (
-    <div className='flex items-center py-1.5 px-4 text-xs md:text-sm space-x-5 w-full'>
-      <div className='w-1/12'>
-        <ViewerAvatar />
-      </div>
-
-      <Column
-        className='w-8/12 max-w-[65%] min-w-[65%]'
-        text='Fronshaizer Andrei'
-      />
-
-      <div className='w-1/12 flex justify-center'>12:51</div>
-
-      <div className='w-1/12 flex justify-center'>
-        <Checkbox />
-      </div>
-    </div>
-  )
-}
