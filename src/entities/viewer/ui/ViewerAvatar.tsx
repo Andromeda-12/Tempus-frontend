3555c7538cc5f18b28d588ccc019ab4d26a671eb
Andromeda <andrei.froschgaizer@yandex.ru>
2023-05-25 21:35:51 +0700

    removed comment

--- src/entities/viewer/ui/ViewerAvatar.tsx
+++ src/entities/viewer/ui/ViewerAvatar.tsx
@@ -12,11 +12,11 @@ export const ViewerAvatar = ({ className, size }: ViewerAvatar) => {
   const viewer = useUnit($viewer)
   let fallback = 'AF'
   if (viewer) fallback = viewer?.firstName[0] + viewer?.lastName[0]
+  
   return (
     <Avatar
       size={size}
       src={getImageUrl(viewer?.avatar)}
-      // src='https://pictures.telegram-store.com/channels/anime-media/38830_2021_11_20_5_.jpg'
       fallback={fallback}
       className={className}
     />
