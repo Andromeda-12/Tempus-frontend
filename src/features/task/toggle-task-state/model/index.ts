e783040e2b547ad7aa081a4594c3b4b6b4d214c6
Andromeda <andrei.froschgaizer@yandex.ru>
2023-05-25 15:14:25 +0700

    refactoring toggle task state feature for local update task

--- src/features/task/toggle-task-state/model/index.ts
+++ src/features/task/toggle-task-state/model/index.ts
@@ -4,6 +4,7 @@ import { currentTaskModel } from '@/entities/current-task'
 import { currentWorkspaceModel } from '@/entities/current-workspace'
 import { currentProjectModel } from '@/entities/current-project'
 import { notificationModel } from '@/features/notification'
+import { viewerModel } from '@/entities/viewer'
 
 export const toggleTaskState = createEvent()
 const toggleTaskStateWithValue = createEvent<{ isRunning: boolean }>()
@@ -56,6 +57,43 @@ sample({
 })
 
 sample({
+  clock: [
+    currentTaskModel.runTaskFx.doneData,
+    currentTaskModel.pauseTaskFx.doneData
+  ],
+  source: {
+    currentTask: currentTaskModel.$currentTask,
+    viewer: viewerModel.$viewer
+  },
+  filter: ({ currentTask, viewer }) => !!currentTask || !!viewer,
+  fn: ({ currentTask, viewer }, memberProgress) => {
+    const changedMembers = structuredClone(currentTask!.members)
+    const viewerIndex = changedMembers!.findIndex(
+      (memberInfo) => memberInfo.member.id === viewer!.id
+    )
+    changedMembers[viewerIndex].workTime = memberProgress.trackedTime
+    changedMembers[viewerIndex].isComplete = memberProgress.isComplete
+
+    return {
+      ...currentTask!,
+      members: changedMembers
+    }
+  },
+  target: currentTaskModel.setCurrentTask
+})
+
+sample({
+  clock: currentTaskModel.runTaskFx.failData,
+  fn: (error) =>
+    notificationModel.createNotificationBody({
+      type: 'warning',
+      title: 'Start task error',
+      message: error.body.message
+    }),
+  target: notificationModel.createNotification
+})
+
+sample({
   clock: currentTaskModel.pauseTaskFx.failData,
   fn: (error) =>
     notificationModel.createNotificationBody({
@@ -65,4 +103,3 @@ sample({
     }),
   target: notificationModel.createNotification
 })
-
